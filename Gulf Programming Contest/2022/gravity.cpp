cb1832f34c830de1b22c8a47f97de57b28c0b821
Meer Husamuddin <meerhusamm@gmail.com>
2024-03-09 10:06:12 +0300

    [GPC] Your Weight on Other Worlds(C++)

---
+++ Gulf Programming Contest/2022/gravity.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip> // For std::setprecision
+
+using namespace std;
+
+int main() {
+    int tCount;
+    cin >> tCount;
+
+    for(int a = 0; a < tCount; a++) {
+        int weightEarth;
+        string body;
+        float weightBody;
+        
+        // Reading the input values
+        cin >> weightEarth >> body >> weightBody;
+        
+        // Setting precision for floating point output
+        cout << fixed << setprecision(1);
+        
+        // Calculating and printing the result
+        cout << "Earth " << weightEarth * 10 << "N " << body << " " << weightBody * weightEarth << "N" << endl;
+    }
+
+    return 0;
+}
