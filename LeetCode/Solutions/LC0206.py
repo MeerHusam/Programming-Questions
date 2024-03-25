ce70aa5ea44c15174a4e233371193ac06a762b3d
Meer Husamuddin <meerhusamm@gmail.com>
2024-03-25 17:06:41 +0300

    [LC] Reverse LinkedList

---
+++ LeetCode/Solutions/LC0206.py
@@ -0,0 +1,21 @@
+"""
+    Problem Statement: https://leetcode.com/problems/reverse-linked-list
+    Author: Meer Husamuddin
+
+    Time Complexity: O(N)
+    Space Complexity: O(1)
+"""
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        while head:
+            temp = head.next
+            head.next = prev
+            prev = head
+            head = temp
+        return prev
