3119ec05da4156accbcd32f17e414f4ecf3b988b
Meer Husamuddin <meerhusamm@gmail.com>
2024-04-05 05:39:00 +0300

    Partitioning Into Minimum Number Of Deci-Binary Numbers

---
+++ LeetCode/Solutions/LC1689.py
@@ -0,0 +1,10 @@
+"""
+    Problem Statement: https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
+    Author: Meer Husamuddin
+
+    Time Complexity: O(N)
+    Space Complexity: O(1)
+"""
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return int(max(n))
