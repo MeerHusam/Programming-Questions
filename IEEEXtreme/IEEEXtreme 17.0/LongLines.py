83df47bad4eab78166db1601c24ce40721604796
Meer Husamuddin <meerhusamm@gmail.com>
2024-08-06 13:01:54 +0300

    [IEEEXtreme] Long Lines

--- IEEEXtreme/IEEEXtreme 17.0/LongLines.py
+++ IEEEXtreme/IEEEXtreme 17.0/LongLines.py
@@ -6,24 +6,23 @@ def find_heights(n, h0, a, c, q):
     return heights
 
 def noticeable_count(heights):
-    i = 0
-    j = 1
+    i, j = 0, 1
     count = 0
+    # Iterate until both i and j do not reach the end
     while(i < len(heights) and j < len(heights) and i != j):
-        if heights[j] >= heights[i]:
-            count += 1
+        # if j is larger or equal to i, increase i as all heights to the left of i
+        # will not be visible OR if j is at the end, and the height is smaller than i, increase i
+        if heights[j] >= heights[i] or (j == len(heights) - 1 and heights[j] < heights[i]):
             i += 1
-            j = i
-        else:
-            count += 1
-        
-        
-        if j == len(heights) - 1 and heights[j] < heights[i]:
-            i += 1
-            j = i
-        if j != len(heights) - 1:
+            j = min(i + 1, len(heights))
+        # increase j if j is smaller than i and j is not at the end
+        elif heights[j] < heights[i]:
             j += 1
-        # print(i, j, count)
+            
+        # Count increases in each iteration because no matter what, a height to 
+        # the immediate left is visible to the right element
+        count += 1
+
     return count
 
 n, h0, a, c, q = map(int, input().split())
